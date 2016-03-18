299b335a98e117e48cc02257407e4bcababc2af0
Fenn-CS <fenn25.fn@gmail.com>
2016-03-18 21:55:39 +0100

    Test Class added, to run simple test cases

---
+++ src/binary/TestCase.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binary;
+import  static org.junit.Assert.*;
+import  org.junit.Test;
+
+/**
+ *
+ * @author fenn
+ */
+public class TestCase{
+   @Test
+   public void Base2Conversion(){
+   }
+   @Test
+   public void Base8Conversion(){
+       
+   } 
+   @Test
+   public void Base10Conversion(){
+       
+   }
+   @Test
+   public void Base16Conversion(){
+   }
+   @Test
+   public void Base32Conversion(){
+       
+   }
+       
+    
+}
